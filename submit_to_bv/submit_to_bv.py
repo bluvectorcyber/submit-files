843dc0534c37bd034f5e8ad01f6aaf0fd7cbea9e
Scott Miserendino <scott.miserendino@bluvector.io>
2019-09-18 10:07:25 -0400

    Refs #BCD-9150: Address reviewer comments

--- submit_to_bv/submit_to_bv.py
+++ submit_to_bv/submit_to_bv.py
@@ -18,7 +18,6 @@ import argparse
 import logging
 import os
 import os.path
-import sys
 import json
 import requests
 
@@ -102,7 +101,7 @@ class SubmitToBV():
         return json.loads(response.content)
 
 
-def my_arg_parser(args):
+def my_arg_parser():
     parser = argparse.ArgumentParser(
         description=(
             "Gathering files wished to be sent to BluVector"))
@@ -130,20 +129,20 @@ def my_arg_parser(args):
         default="api.bluvector.io",
         help=(
             "Hostname of the Submit to BluVector service (default: 'api.bluvector.io')"))
-    args = parser.parse_args(args)
-    return args
+    my_args = parser.parse_args()
+    return my_args
 
 
-def cli(args):
-    client = SubmitToBV(args.username, args.password, 
-                        log=args.log_filename, server_hostname=args.server_hostname)
-    client.submit(args.input_path)
+def cli(my_args):
+    client = SubmitToBV(my_args.username, my_args.password,
+                        log=my_args.log_filename, server_hostname=my_args.server_hostname)
+    client.submit(my_args.input_path)
     print("Done")
 
 
 def main():
-    args = my_arg_parser(sys.argv[1:])
-    cli(args)
+    my_args = my_arg_parser()
+    cli(my_args)
 
 
 if __name__ == "__main__":
